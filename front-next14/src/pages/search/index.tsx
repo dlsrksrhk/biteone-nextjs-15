99266bc3c5dc1d00095f74b822e3412575c2a124
sweet.dh <dlsrksrhk@gmail.com>
2025-04-14 15:24:34 +0900

    레이아웃 mock 데이터로 완성

--- front-next14/src/pages/search/index.tsx
+++ front-next14/src/pages/search/index.tsx
@@ -1,13 +1,16 @@
 import SearchableLayout from "@/components/searchable-layout";
-import { useRouter } from "next/router";
 import { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-  const { greeting } = router.query;
-
-  console.log("greeting", greeting);
-  return <h1>Search {greeting}</h1>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
 
 Page.getLayout = (page: ReactNode) => {
