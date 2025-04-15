dba76fbc60eeb64604a70a8de21d301c40407c27
sweet.dh <dlsrksrhk@gmail.com>
2025-04-15 16:55:13 +0900

    SEO 최적화
    title, thumbnail, meta태그 추가

--- front-next14/src/pages/index.tsx
+++ front-next14/src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   console.log("getStaticProps!!!");
@@ -26,20 +27,31 @@ export default function Home({
   recomBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recomBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>지금 등록된 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>스윗북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="스윗북스" />
+        <meta
+          property="og:description"
+          content="당신의 도서관을 만들어보세요!"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recomBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>지금 등록된 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
