9b548eaf5628aafb1a03eea8daa117e1875a8d89
sweet.dh <dlsrksrhk@gmail.com>
2025-04-16 20:57:45 +0900

    api서버로부터 fetch 해서 SSR 처리

--- front-next15/src/app/(with-searchbar)/search/page.tsx
+++ front-next15/src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,23 @@
 import BookItem from "@/components/book-item";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
 
-export default function Page({
+export default async function Page({
   searchParams,
 }: {
   searchParams: Promise<{
     q?: string;
   }>;
 }) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`
+  );
+
+  if (!response.ok) {
+    return <div>어머 ! 에러가 발생했어요.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
       {books.map((book) => (
